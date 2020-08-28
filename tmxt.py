98b7707bebefea03363bb5a2883c3688d78a4414
Jaume Zaragoza <jzaragoza@prompsit.com>
2020-08-28 15:45:53 +0200

    Allow TU propierties to be printed if requested

--- tmxt.py
+++ tmxt.py
@@ -6,6 +6,7 @@ Usage:
 
 Options:
   --codelist=<langcodes>   Comma-separated list of langcodes (i.e. "en,es").
+                           TU priopierties can also be specified
   
 I/O Defaults:
   INPUT_FILE               Defaults to stdin.
@@ -22,12 +23,15 @@ def process_tmx(input, output, codelist):
     curtuv   = []
     intuv    = False
     tu       = {}
+    intprop  = True
+    curtype  = ""
+    curprop  = []
     p1       = re.compile(r'\n')
     p2       = re.compile(r'  *')    
-    fmt      = ("{}\t"*len(codelist)).strip()+"\n"
+    fmt      = ("{}\t"*(len(codelist))).strip()+"\n"
 
     def se(name, attrs):
-        nonlocal intuv, curtuv, tu, curlang, codelist
+        nonlocal intuv, intprop, curtuv, curprop, curtype, tu, curlang, codelist
         if intuv:
             curtuv.append("")
         elif name == "tu":
@@ -40,9 +44,13 @@ def process_tmx(input, output, codelist):
         elif name == "seg":
             curtuv = []
             intuv = True
+        elif name == "prop":
+            intprop = True
+            curtype = attrs['type']
+            curprop = []
             
     def ee(name):
-        nonlocal intuv, curtuv, p1, p2, tu, curlang, codelist, fmt, output
+        nonlocal intuv, intprop, curtuv, curprop, curtype, p1, p2, tu, curlang, codelist, fmt, output
         if name == "tu":
             output.write(fmt.format(*[tu[lang] for lang in codelist]))
 
@@ -51,11 +59,18 @@ def process_tmx(input, output, codelist):
             mystr = p2.sub(' ', p1.sub(' ', "".join(curtuv))).strip()
             tu[curlang] = mystr
             curlang = ""
+        elif name == "prop":
+            introp = False
+            mystr = p2.sub(' ', p1.sub(' ', "".join(curprop))).strip()
+            tu[curtype] = mystr
+            curtype = ""
     
     def cd(data):
-        nonlocal intuv, curtuv
+        nonlocal intuv, curtuv, intprop, curprop
         if intuv:
             curtuv.append(data)
+        if intprop:
+            curprop.append(data)
 
     p = xml.parsers.expat.ParserCreate()
     p.StartElementHandler  = se
@@ -64,7 +79,7 @@ def process_tmx(input, output, codelist):
     p.ParseFile(input) 
 
 if __name__ == '__main__':
-    arguments = docopt(__doc__, version='tmxt 1.0')
+    arguments = docopt(__doc__, version='tmxt 1.1')
     
     input = sys.stdin.buffer if not arguments["INPUT_FILE"] else open(arguments["INPUT_FILE"], "rb")
     output = sys.stdout if not arguments["OUTPUT_FILE"] else open(arguments["OUTPUT_FILE"], "w")    
